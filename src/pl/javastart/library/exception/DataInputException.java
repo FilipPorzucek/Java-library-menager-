0c3ddc47fc2f714dfcaca228611dab098757bc32
FilipPorzucek <147589167+FilipPorzucek@users.noreply.github.com>
2025-02-06 14:15:12 +0100

    Delete src/pl/javastart/library directory

--- src/pl/javastart/library/exception/DataInputException.java
+++
@@ -1,7 +0,0 @@
-package pl.javastart.library.exception;
-
-public class DataInputException extends RuntimeException{
-    public DataInputException(String message) {
-        super(message);
-    }
-}
--- src/pl/javastart/library/io/file/FileMenager.java
+++
@@ -1,9 +0,0 @@
-package pl.javastart.library.io.file;
-
-import pl.javastart.library.model.Library;
-
-public interface FileMenager {
-    Library importData();
-    void exportData(Library library);
-
-}
